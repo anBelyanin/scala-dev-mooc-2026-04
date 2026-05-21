51ecd36ae1e162822434ce050e8115220e635b70
AndreiBelianin <abelianin@unitarius.ru>
2026-05-21 23:17:17 +0300

    complete homework

--- src/main/scala/ru/otus/module1/03-introducing_fp_concepts.scala
+++ src/main/scala/ru/otus/module1/03-introducing_fp_concepts.scala
@@ -2,8 +2,9 @@ package ru.otus.module1
 
 
 
-import ru.otus.module1.variance.{Animal, Cat}
+import ru.otus.module1.variance.{Animal, Cat, animalFeeder}
 
+import java.util.Optional
 import scala.language.postfixOps
 
 
@@ -50,7 +51,16 @@ object recursion {
    * Реализовать вычисление N числа Фибоначчи
    * F0 = 0, F1 = 1, Fn = Fn-1 + Fn - 2
    */
-
+  def fibonacciDigit(n: Int): Int = {
+    def calculate(index: Int, past1: Int, past2: Int): Int = {
+      val sum: Int = past1 + past2
+      if (index == n) sum
+      else calculate(index + 1, sum, past1)
+    }
+    if (n == 0) 0
+    else if (n == 1) 1
+    else calculate(2, 1, 0)
+  }
 
 }
 
@@ -164,6 +174,31 @@ object variance {
     def flatMap[B](f: T => Option[B]): Option[B] =
       if (isEmpty) None
       else f(this.asInstanceOf[Some[T]].v)
+
+    /**
+     *
+     * Реализовать метод printIfAny, который будет печатать значение, если оно есть
+     */
+    def printIfAny(): Unit = if (!isEmpty) println(this.asInstanceOf[Some[T]].v)
+
+    /**
+     *
+     * Реализовать метод zip, который будет создавать Option от пары значений из 2-х Option
+     */
+    def zip[TT >: T, B](augend: Option[TT], zipFunc: (opt1: Option[T], opt2: Option[TT]) => Option[B]): Option[B] = {
+      if (augend.isEmpty) zipFunc(this, None)
+      else zipFunc(this, augend)
+    }
+
+    /**
+     *
+     * Реализовать метод filter, который будет возвращать не пустой Option
+     * в случае если исходный не пуст и предикат от значения = true
+     */
+    def filter(predicate: (opt: T) => Boolean): Option[T] = {
+      if (!isEmpty && predicate(this.asInstanceOf[Some[T]].v)) this
+      else None
+    }
   }
 
   object Option {
@@ -176,27 +211,6 @@ object variance {
   var animalOpt: Option[Animal] = None
   var intOpt: Option[Int] = ???
 
-
-
-
-  /**
-   *
-   * Реализовать метод printIfAny, который будет печатать значение, если оно есть
-   */
-
-
-  /**
-   *
-   * Реализовать метод zip, который будет создавать Option от пары значений из 2-х Option
-   */
-
-
-  /**
-   *
-   * Реализовать метод filter, который будет возвращать не пустой Option
-   * в случае если исходный не пуст и предикат от значения = true
-   */
-
  }
 
  object list {
@@ -208,60 +222,93 @@ object variance {
     * Cons - непустой, содержит первый элемент (голову) и хвост (оставшийся список)
     */
 
+   sealed trait List[+T] {
+     def ::[TT >: T](elem: TT): List[TT] = new::[TT](elem, this)
 
-    sealed trait List[+T]{
-      def ::[TT >: T](elem: TT): List[TT] = ???
-    }
-
-
-    case class ::[A](head: A, tail: List[A]) extends List[A]
-    case object Nil extends List[Nothing]
-
-    object List {
-      def apply[A](v: A*): List[A] =
-        if(v.isEmpty) Nil else ::(v.head, apply(v.tail:_*))
-    }
-
-
-
-
-
-
-    /**
-      * Конструктор, позволяющий создать список из N - го числа аргументов
-      * Для этого можно воспользоваться *
-      * 
-      * Например, вот этот метод принимает некую последовательность аргументов с типом Int и выводит их на печать
-      * def printArgs(args: Int*) = args.foreach(println(_))
-      */
-
-    /**
+     /**
       *
       * Реализовать метод reverse который позволит заменить порядок элементов в списке на противоположный
       */
+     def reverse[TT >: T](): List[TT] = {
+       def doReverse(element: List[TT], accumulator: List[TT]): List[TT] = element match {
+         case Nil => accumulator
+         case ::(head: TT, tail: List[TT]) => doReverse(tail, head :: accumulator)
+       }
 
-    /**
+       doReverse(this, Nil)
+     }
+
+
+     /**
       *
       * Реализовать метод map для списка который будет применять некую ф-цию к элементам данного списка
       */
+     def map[B](f: T => B): List[B] = {
+       def doMap(element: List[T], accumulator: List[B]): List[B] = {
+         if (element.isInstanceOf[Nil.type]) accumulator.reverse()
+         else {
+           val cons = element.asInstanceOf[::[T]]
+           doMap(cons.tail, f(cons.head) :: accumulator)
+         }
+       }
 
+       doMap(this, Nil)
+     }
 
-    /**
+     /**
       *
       * Реализовать метод filter для списка который будет фильтровать список по некому условию
       */
+     def filter[T](predicate: T => Boolean): List[T] = {
+       def doFilter(head: T, tail: List[T], accumulator: List[T]): List[T] = {
+         if (tail.isInstanceOf[Nil.type]) accumulator
+         else {
+           val tailCons = tail.asInstanceOf[::[T]]
+           if (predicate(head)) {
+             doFilter(tailCons.head, tailCons.tail, head :: accumulator)
+           }
+           else doFilter(tailCons.head, tailCons.tail, accumulator)
+         }
+       }
+
+       this match {
+         case thisCons: ::[T] =>
+           doFilter(thisCons.head, thisCons.tail, Nil)
+         case _ => Nil
+       }
+     }
+
+   }
+
+
+   case class ::[A](head: A, tail: List[A]) extends List[A]
+
+   case object Nil extends List[Nothing]
+
+   object List {
+     def apply[A](v: A*): List[A] =
+       if (v.isEmpty) Nil else ::(v.head, apply(v.tail: _*))
+   }
+
+   /**
+    *
+    * Написать функцию incList которая будет принимать список Int и возвращать список,
+    * где каждый элемент будет увеличен на 1
+    */
+   val incList: List[Int] => List[Int] = _.map(element => element + 1)
+
+   /**
+    *
+    * Написать функцию shoutString которая будет принимать список String и возвращать список,
+    * где к каждому элементу будет добавлен префикс в виде '!'
+    */
+   val shoutString: List[String] => List[String] = _.map(element => "!".concat(element))
 
     /**
-      *
-      * Написать функцию incList которая будет принимать список Int и возвращать список,
-      * где каждый элемент будет увеличен на 1
-      */
-
-
-    /**
-      *
-      * Написать функцию shoutString которая будет принимать список String и возвращать список,
-      * где к каждому элементу будет добавлен префикс в виде '!'
-      */
-
+     * Конструктор, позволяющий создать список из N - го числа аргументов
+     * Для этого можно воспользоваться *
+     *
+     * Например, вот этот метод принимает некую последовательность аргументов с типом Int и выводит их на печать
+     * def printArgs(args: Int*) = args.foreach(println(_))
+     */
  }
